f2057a9427dcc756cb4600cc90912a262bb80f6c
Dharani Yedavelly <dharani1414@gmail.com>
2026-02-04 15:01:21 -0600

    Refactor API code

--- tests/WebAPIPart1test.spec.js
+++ tests/WebAPIPart1test.spec.js
@@ -1,30 +1,43 @@
 const {test, expect, request} = require('@playwright/test');
-const loginPayLoad = {userEmail:'playwright1414@gmail.com', userPassword:'Test1414'};
-let token;
-
-test.beforeAll( async ()=>
+const {APiUtils} = require('./utils/APiUtils');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
+ 
+ 
+let response;
+test.beforeAll( async()=>
 {
-const apiContext = await request.newContext();
-//Grabbing the token
-const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+ 
+})
+ 
+ 
+//create order is success
+test('@API Place the order', async ({page})=>
 { 
-    data:loginPayLoad 
-});
-  expect(loginResponse.ok()).toBeTruthy();
-  const loginResponseJson = await loginResponse.json();
-  token = loginResponseJson.token;
-  console.log(token);
-});
-
-
-test('Login with token', async({page})=>{
-
-
-//Setting up the token in local storage
-await page.addInitScript(value => {
-     window.localStorage.setItem('token', value);
-},token);
-await page.goto("https://rahulshettyacademy.com/client/");
-
-
+    await page.addInitScript(value => {
+ 
+        window.localStorage.setItem('token',value);
+    }, response.token );
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.locator("button[routerlink*='myorders']").click();
+ await page.locator("tbody").waitFor();
+const rows = await page.locator("tbody tr");
+ 
+ 
+for(let i =0; i<await rows.count(); ++i)
+{
+   const rowOrderId =await rows.nth(i).locator("th").textContent();
+   if (response.orderId.includes(rowOrderId))
+   {
+       await rows.nth(i).locator("button").first().click();
+       break;
+   }
+}
+const orderIdDetails =await page.locator(".col-text").textContent();
+//await page.pause();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+ 
 });
